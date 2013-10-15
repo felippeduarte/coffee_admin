1925889afef4ca76b854faa8e42a8fe49863a70d
Felippe Roberto Bayestorff Duarte <frbduarte@ciasc.sc.gov.br>
2013-10-15 17:54:56 -0300

    Alteração nos lançamentos para exibir grid em view separada

--- protected/views/lancamento/index.php
+++ protected/views/lancamento/index.php
@@ -7,116 +7,25 @@ $this->breadcrumbs=array(
 
 <h1>Lançamentos</h1>
 
-<?php 
-$dataProvider = $modelLancamento->getLancamentoGrid();
-$dataProvider->pagination = array('pagesize'=>10);
-$dataProvider->sort = array(
-            'attributes'=>array(
-                'id_lancamento'=>array(
-                    'asc'=>'id_lancamento',
-                    'desc'=>'id_lancamento DESC',
-                ),
-                'dt_lancamento'=>array(
-                    'asc'=>'dt_lancamento',
-                    'desc'=>'dt_lancamento DESC',
-                ),
-                'nm_estabelecimento'=>array(
-                    'asc'=>'idEstabelecimento.nm_estabelecimento',
-                    'desc'=>'idEstabelecimento.nm_estabelecimento DESC',
-                ),
-                'nm_categoriaLancamento'=>array(
-                    'asc'=>'idCategoriaLancamento.nm_categoriaLancamento',
-                    'desc'=>'idCategoriaLancamento.nm_categoriaLancamento DESC',
-                ),
-                'nm_pessoa'=>array(
-                    'asc'=>'idPessoaLancamento.nm_pessoa',
-                    'desc'=>'idPessoaLancamento.nm_pessoa DESC',
-                ),
-                'vl_lancamento'=>array(
-                    'asc'=>'vl_lancamento',
-                    'desc'=>'vl_lancamento DESC',
-                ),
-                '*',
-            ));
-
-$gridColumns = array(
-      array(
-        'header' => '#',
-        'name'  => 'id_lancamento',
-        'htmlOptions'=>array('style'=>'width: 5px')
-        ),
+<?php
+$form = $this->beginWidget(
+    'bootstrap.widgets.TbActiveForm',
     array(
-        'header' => 'Data',
-        'name'  => 'dt_lancamento',
-        'value' => 'Yii::app()->dateFormatter->format("dd/MM/yyyy",$data->dt_lancamento)',
-        'htmlOptions'=>array('style'=>'width: 20px'),
-        'class' => 'bootstrap.widgets.TbEditableColumn',
-        'editable' => array(
-				'url' => $this->createUrl('lancamento/edit'),
-				'placement' => 'right',
-				'inputclass' => 'span3'
-			),
-        ),
-    array(
-        'header' => 'Estabelecimento',
-        'name'  => 'nm_estabelecimento',
-        'value'  => '$data->idEstabelecimento->nm_estabelecimento',
-        'htmlOptions'=>array('style'=>'width: 60px')
-        ),
-    array(
-        'header' => 'Categoria',
-        'name'  => 'nm_categoriaLancamento',
-        'value'  => '$data->idCategoriaLancamento->nm_categoriaLancamento',
-        'htmlOptions'=>array('style'=>'width: 100px')
-        ),
-    array(
-        'header' => '',
-        'name'  => 'idCategoriaLancamento.tp_categoriaLancamento',
-        'htmlOptions'=>array('style'=>'width: 5px')
-        ),
-    array(
-        'header' => 'Favorecido',
-        'name'  => 'nm_pessoa',
-        'value'  => '$data->idPessoaLancamento->nm_pessoa',
-        'htmlOptions'=>array('style'=>'width: 60px')
-        ),
-    array(
-        'header' => 'Valor',
-        'name'  => 'vl_lancamento',
-        'htmlOptions'=>array('style'=>'width: 30px')
-        ),
-    array(
-		'htmlOptions' => array('nowrap'=>'nowrap','style'=>'10px'),
-		'class'=>'bootstrap.widgets.TbButtonColumn',
-        'template'=>'{update}{delete}',
-        'deleteConfirmation'=>false,
-        'buttons'=>array(
-            'update' => array(
-                'url'=>null,
-                'options'=>array(
-                    'class'=>'update',
-                    'data-toggle' => 'modal',
-                ),
-            ),
-            'delete' => array(
-                'url' => null,
-                'options'=>array(
-                    'class'=>'delete',
-                )
-            )
-        ),
+        'id' => 'verticalForm',
+        'htmlOptions' => array('class' => 'well'), // for inset effect
     )
 );
+echo $form->maskedTextFieldRow($modelLancamento,'dt_lancamento','99/99/9999',
+                        array('prepend'=>'<i class="icon-calendar"></i>',
+                              'class' => 'input-small',
+                              'value' => date('d/m/Y')
+                        )
+                    ); 
 
-$this->widget('bootstrap.widgets.TbExtendedGridView', array(
-    'id'=>'gridLancamentos',
-	'filter'=>$modelLancamento,
-	'fixedHeader' => true,
-	'headerOffset' => 40, // 40px is the height of the main navigation at bootstrap
-	'type'=>'striped bordered',
-    'enablePagination' => true,
-	'dataProvider' => $dataProvider,
-	'template' => "{items}\n{pager}",
-	'columns' => $gridColumns,
-));
-?>
+$this->widget(
+    'bootstrap.widgets.TbButton',
+    array('buttonType' => 'submit', 'label' => 'Pesquisar')
+);
+ 
+$this->endWidget();
+unset($form);
